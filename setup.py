3cb28cdfb048331bf8c526ed6d49903a6dfebd1c
mv <michvett.mv@gmail.com>
2019-03-08 10:32:18 +0100

    restructure project folder

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize(["py_pbf_gui.py","py_pbf_helper.py","py_pbf_gui_sub.py"])
-)    
+    ext_modules=cythonize(["py_pbf/py_pbf_gui.py","py_pbf/py_pbf_helper.py","py_pbf/py_pbf_gui_sub.py"])
+)
